e30396d34f626e58a07a128b1627b0c6abe19832
OguzTNCR <100152836+OguzTNCR@users.noreply.github.com>
2023-03-17 15:42:34 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct  {
+    int type;
+    char *value;
+}Token;
+
+Token* tokenize(char* line) {
+    int type;
+    char* start = line;
+    while (*line == ' ') {   // skipping whitespaces
+        line ++;
+        if (*line == '\0') {   // end of line
+            break;
+        }
+    }
+    if (*line == '+') {
+        type = 2;
+        line++;
+    }
+    else if (isdigit(*line)) {
+        type = 1;
+        while (isdigit(*line)) {
+            *start = *line;
+            line++;
+        }
+    }
+
+    Token* token = malloc(sizeof(Token));
+    token -> type = type;
+    token -> value = strndup(start, line - start);
+    return token;
+
+
+
+}
+
+
+int main() {
+
+
+    while (1) {
+        char line[256];
+
+        printf(">");
+        fgets(line, 256, stdin);
+        Token *token = tokenize(line);
+        printf("type = %d : value %s\n", token -> type, token -> value);
+
+    }
+}
